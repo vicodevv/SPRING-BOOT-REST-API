23df45fc48e76c2642da1d318690669f20c7eb36
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-09-16 01:26:34 +0100

    create student class

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,68 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private long id;
+    private String name;
+    private Integer age;
+    private LocalDate dob;
+    private String email;
+
+    public Student(){
+    }
+
+    public Student(Long id,
+                   String name,
+                   Integer age,
+                   LocalDate dob,
+                   String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public Student(String name,
+                   Integer age,
+                   LocalDate dob,
+                   String email) {
+        this.name = name;
+        this.age = age;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getAge() {
+        return age;
+    }
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    public LocalDate getDob() {
+        return dob;
+    }
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
