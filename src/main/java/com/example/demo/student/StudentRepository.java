c13b0adc249be1e53a3bb3ed0b4a68fcb3485fa3
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-09-17 16:53:26 +0100

    email validation

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,10 +1,13 @@
 package com.example.demo.student;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findStudentByEmail(String email);
 
 }
     
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,6 +2,7 @@ package com.example.demo.student;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -20,6 +21,10 @@ public class StudentService {
 		return studentRepository.findAll();
 	}
     public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("User already exists");
+        }
         System.out.println(student);
     }
 }
